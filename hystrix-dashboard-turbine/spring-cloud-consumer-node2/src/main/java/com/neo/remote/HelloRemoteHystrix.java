7e59beaf7b122b8edefd1c5197d1fb5e1150a0ae
wangjian309@126.com <wangjian309@126.com>
2017-09-23 21:59:09 +0800

    增加服务提供者

--- hystrix-dashboard-turbine/spring-cloud-consumer-node2/src/main/java/com/neo/remote/HelloRemoteHystrix.java
+++ hystrix-dashboard-turbine/spring-cloud-consumer-node2/src/main/java/com/neo/remote/HelloRemoteHystrix.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Component
 public class HelloRemoteHystrix implements HelloRemote{
 
-    @Override
     public String hello2(@RequestParam(value = "name") String name) {
         return "hello " +name+", this messge send failed ";
     }
